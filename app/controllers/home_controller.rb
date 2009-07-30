7fc73199a66413198795a79402c0cb0ede70f3bb
tonkapark <matt@tonkapark.com>
2009-07-29 23:58:06 -0500

    base layouts for Items

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
+  
   def index
+    if signed_in?
+      @items = Item.paginate :page => params[:page]
+    end
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  
   map.resources :items
-
   map.root :controller => "home"
 
 end
