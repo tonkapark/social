4c1062fb4713645006099f50270193a3667d1efc
tonkapark <matt@tonkapark.com>
2009-08-03 16:52:30 -0500

    correct name of notifier in tests

--- test/unit/mailer_test.rb
+++ test/unit/mailer_test.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
 class MailerTest < ActionMailer::TestCase
-  test "notify" do
-    @expected.subject = 'Mailer#notify'
-    @expected.body    = read_fixture('notify')
+  test "item_notify" do
+    @expected.subject = 'Mailer#item_notify'
+    @expected.body    = read_fixture('item_notify')
     @expected.date    = Time.now
 
-    assert_equal @expected.encoded, Mailer.create_notify(@expected.date).encoded
+    assert_equal @expected.encoded, Mailer.create_item_notify(@expected.date).encoded
   end
 
 end
